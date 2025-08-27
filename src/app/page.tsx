79deaa6cf36ffc8545e7370a34826e2423181c95
GGronland <glenrgronland@gmail.com>
2025-08-27 18:08:56 +0100

    Updated home page hero to use profile picture. TODO - implement Q/A in about page instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import useWordCycler from "@/components/hooks/useWordCycler";
 import Button from "@/components/ui/Button";
 import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import Image from "next/image";
+import ProfilePicture from "@/assets/profilePicture.jpg";
 
 export default function Home() {
   let title = useWordCycler(
@@ -21,8 +23,8 @@ export default function Home() {
   );
 
   return (
-    <section className="grid grid-cols-[1fr,1fr] items-center justify-items-center p-8 pb-20 gap-10 sm:p-20">
-      <div className="col-start-1 flex flex-col gap-8 w-full  p-8 border-1 rounded-xl box-border font-sans bg-sky-900 border-sky-900">
+    <section className="flex flex-col xl:flex-row items-center justify-items-center p-8 pb-20 gap-10 sm:p-20 animate-fade-in-slow">
+      <div className="col-start-1 flex flex-col gap-8 w-full  p-8 border-1 rounded-xl box-border font-sans bg-slate-700 border-slate-700">
         <div>
           <p className="font-semibold text-xl">Hi, my name is</p>
           <p className="font-bold text-6xl">Glen Gronland</p>
@@ -35,21 +37,29 @@ export default function Home() {
           expanding array of programming languages.
         </p>
 
-        <div className="flex gap-8">
-          <Button className="col-start-1" isCTA>
-            <a href="/assets/files/CV_GlenGronland.pdf" download className="">
+        <div className="flex flex-wrap gap-4">
+          <a href="/assets/files/CV_GlenGronland.pdf" download className="">
+            <Button className="col-start-1" isCTA>
               Download CV
-            </a>
-            <ArrowDownTrayIcon className="w-6 h-6" />
-          </Button>
-          <Button className="col-start-2" isCTA>
-            <Link href="/contact">Contact me</Link>
-          </Button>
+              <ArrowDownTrayIcon className="w-5 h-5" />
+            </Button>
+          </a>
+
+          <Link href="/contact">
+            <Button className="col-start-2" isCTA>
+              Contact me
+            </Button>
+          </Link>
         </div>
       </div>
 
-      <div className="col-start-2 min-w-[400px] h-full border-1 rounded-xl bg-sky-900 border-sky-900">
-        <div>Tech demo container</div>
+      {/* TODO - create "stack" of pictures at different angles that cycle on click */}
+      <div className="col-start-2 max-w-[350px] rounded-xl bg-slate-700 hover:rotate-5 transition-all duration-500 ease-linear">
+        <Image
+          className="box-border border-3 rounded-xl border-slate-700 bg-slate-700 drop-shadow-lg hover:border-green-300 transition-all duration-500 "
+          src={ProfilePicture}
+          alt="Profile picture"
+        />
       </div>
     </section>
   );
